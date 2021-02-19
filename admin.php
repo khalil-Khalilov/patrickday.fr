59932f6a78063d8ba261a4e1863e6ef707217628
Khalil <you@example.com>
2021-02-19 15:12:18 +0100

    khalil

--- admin.php
+++ admin.php
@@ -2,13 +2,16 @@
 require('assets/head.php')
 ?>
 
-<ul>
-    <li><button>Modiffier Mot de passe</button></li>
-    <li><button>Ajouter une image à la gallery</button></li>
-    <li><button>Ajouter une video à la gallery</button></li>
-    <li><button>Voir la liste des images</button></li>
-    <li><button>Voir la liste des videos</button></li>
-</ul>
+<div id="kh-container">
+    <ul id="">
+        <li><button>Modiffier Mot de passe</button></li>
+        <li><button>Ajouter une image à la gallery</button></li>
+        <li><button>Ajouter une video à la gallery</button></li>
+        <li><button>Voir la liste des images</button></li>
+        <li><button>Voir la liste des videos</button></li>
+    </ul>
+</div>
+
 
 
 
