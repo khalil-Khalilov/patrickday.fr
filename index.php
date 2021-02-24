e6bc7aece59f5c5f4a7adff823bea65e7c882cbe
CaroleDEMIT <carole.demit@wanadoo.fr>
2021-02-24 15:47:28 +0100

    images carrousel

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ require('assets/head.php')
 <div id="carousel">
 
     <ul>
-        <li><img src="https://picsum.photos/id/1011/1000/400"/></li>
-        <li><img src="https://picsum.photos/id/1015/1000/400"/></li>
-        <li><img src="https://picsum.photos/id/1039/1000/400"/></li>
-        <li><img src="https://picsum.photos/id/1026/1000/400"/></li>
-        <li><img src="https://picsum.photos/id/1043/1000/400"/></li>
-        <li><img src="https://picsum.photos/id/182/1000/400"/></li>
+        <li><img src="media\images/carrousel/carrousel1.jpg"/></li>
+        <li><img src="media\images/carrousel/carrousel2.jpg"/></li>
+        <li><img src="media\images/carrousel/carrousel3.jpg"/></li>
+        <li><img src="media\images/carrousel/carrousel4.jpg"/></li>
+        <li><img src="media\images/carrousel/carrousel5.jpg"/></li>
+        <li><img src="media\images/carrousel/carrousel6.jpg"/></li>
     </ul>
 
     <i class="fas fa-chevron-circle-right next"></i>
